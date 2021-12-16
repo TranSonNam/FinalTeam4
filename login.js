9f6d2be85357045a1f9e551ad7042db779d34a78
NHnamz <namday9zz@gmail.com>
2021-12-16 20:59:30 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("#loginForm");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  const listUser = JSON.parse(localStorage.getItem("listUser"))
+    ? JSON.parse(localStorage.getItem("listUser"))
+    : [];
+  const check = listUser.findIndex(
+    (item) => item.username == username && item.password == password
+  );
+  localStorage.setItem("userInfo", JSON.stringify(listUser[check]));
+  if (check >= 0) {
+    window.location.href = "./index.html";
+  } else {
+    alert("Sai tên đăng nhập hoặc mật khẩu");
+  }
+});
+
+function register() {
+  window.location.href = "./register.html";
+}
